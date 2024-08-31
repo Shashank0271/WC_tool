a7042b3fce9110f86f11daddb24ad313bc21cfb6
Shashank0271 <ssingh0271@gmail.com>
2024-08-31 18:22:42 +0530

    added support for default command

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { ComputeFile } from "./computeFile.ts";
 
 const input: Array<string> = process.argv;
 
-if (input[2] !== "ccwc" || input.length < 5) {
+if (input[2] !== "ccwc") {
   console.error("invalid command");
   exit(0);
 }
@@ -15,36 +15,50 @@ if (input[2] !== "ccwc" || input.length < 5) {
 const flags: Array<string> = ["-c", "-l", "-w", "-m"];
 
 try {
+  let command: Command;
   if (flags.includes(input[3] as string)) {
-    const command: Command = {
+    command = {
       commandType: findCommandType(input),
       filePath: input[4] as string,
     };
+  } else {
+    command = {
+      commandType: CommandType.DEFAULT,
+      filePath: input[3] as string,
+    };
+  }
 
-    const computeFile = new ComputeFile(command.filePath);
-    await computeFile.readFileContent();
+  const computeFile = new ComputeFile(command.filePath);
+  await computeFile.readFileContent();
 
-    switch (command.commandType) {
-      case CommandType.READ_BYTES:
-        const bytes = await computeFile.calculateBytes();
-        console.log(bytes + " " + command.filePath);
-        break;
+  switch (command.commandType) {
+    case CommandType.READ_BYTES:
+      const bytes = await computeFile.calculateBytes();
+      console.log(bytes + " " + command.filePath);
+      break;
 
-      case CommandType.READ_LINES:
-        const lines = await computeFile.calculateLines();
-        console.log(lines + " " + command.filePath);
-        break;
+    case CommandType.READ_LINES:
+      const lines = await computeFile.calculateLines();
+      console.log(lines + " " + command.filePath);
+      break;
 
-      case CommandType.READ_CHARS:
-        const characters = await computeFile.calculateWords();
-        console.log(characters + " " + command.filePath);
-        break;
+    case CommandType.READ_CHARS:
+      const characters = await computeFile.calculateWords();
+      console.log(characters + " " + command.filePath);
+      break;
 
-      case CommandType.READ_WORDS:
-        const words = await computeFile.calculateWords();
-        console.log(words + " " + command.filePath);
-        break;
-    }
+    case CommandType.READ_WORDS:
+      const words = await computeFile.calculateWords();
+      console.log(words + " " + command.filePath);
+      break;
+
+    case CommandType.DEFAULT:
+      const fileLines = await computeFile.calculateLines();
+      const fileWords = await computeFile.calculateWords();
+      const fileBytes = await computeFile.calculateBytes();
+      console.log(
+        fileLines + " " + fileWords + " " + fileBytes + " " + command.filePath
+      );
   }
 } catch (e: any) {
   console.log(e.toString());
